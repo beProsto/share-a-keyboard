771441b55b9989d793a6d1cbf55d0c4acb837e6a
beProsto <lavionperavion@gmail.com>
2023-11-07 01:45:29 +0100

    [src] windows client receives and simulates keystrokes properly lol

--- src/client/client.c
+++ src/client/client.c
@@ -1,4 +1,5 @@
 #include <share-a-keyb/networking.h>
+#include <share-a-keyb/keyboard.h>
 #include <stdio.h>
 
 #define DESIRED_ADDRESS "127.0.0.1"
@@ -23,8 +24,8 @@ int main(int argc, char **argv) {
   }
 
   while (true) {
-    int temp_arr[1] = {0};
-    int received_size = recv(sock, temp_arr, sizeof(temp_arr), 0);
+    struct key_input_info keyinputinfo = {0};
+    int received_size = recv(sock, &keyinputinfo, sizeof(keyinputinfo), 0);
     if (received_size == 0) {
       printf("connection ended by server\n");
       return 3;
@@ -33,44 +34,57 @@ int main(int argc, char **argv) {
       return 4;
     }
 
-    printf("%d Received Key Press: [%d] = '%c'\n", received_size, temp_arr[0],
-           temp_arr[0]);
+    int caughtScanCode = keyinputinfo.scancode;
 
-#ifndef WIN32
-    // TODO: Handle linux keyboard simulation
-#else 
+    unsigned int vkey = MapVirtualKeyA(caughtScanCode, MAPVK_VSC_TO_VK);
+
+    printf("(%d) Received Key Press: [%d] = v[%d] = '%c'\n", keyinputinfo.eventtype, received_size, caughtScanCode, vkey,
+           vkey);
+
+// #ifndef WIN32
+//     // TODO: Handle linux keyboard simulation
+// #else 
     // prepare the caught input for windows
-    int caughtKey = temp_arr[0];
     
-    INPUT inputs[3] = {0};
-    uint8_t strokes = 1;
-    for(int i = 0; i < (sizeof(inputs)/sizeof(INPUT)); i++) {
-      inputs[i].type = INPUT_KEYBOARD;
-    }
+    // INPUT inputs[3] = {0};
+    // uint8_t strokes = 1;
+    // for(int i = 0; i < (sizeof(inputs)/sizeof(INPUT)); i++) {
+    //   inputs[i].type = INPUT_KEYBOARD;
+    // }
 
-    if(caughtKey == 127) { // backspace
-      inputs[0].ki.wVk = VK_BACK;
-    }
-    else if(caughtKey == 23) { // ctrl+backspace
-      strokes = 3;
-      inputs[0].ki.wVk = VK_CONTROL;
-      inputs[1].ki.wVk = VK_BACK;
-      inputs[2].ki.wVk = VK_CONTROL;
-      inputs[2].ki.dwFlags = KEYEVENTF_KEYUP;
-    }
-    else if(caughtKey == 10) { // enter
-      inputs[0].ki.wVk = VK_RETURN;
-    }
-    else {
-      inputs[0].ki.wScan = caughtKey;
-      inputs[0].ki.dwFlags = KEYEVENTF_UNICODE;
-    }
+    // if(caughtKey == 127) { // backspace
+    //   inputs[0].ki.wVk = VK_BACK;
+    // }
+    // else if(caughtKey == 23) { // ctrl+backspace
+    //   strokes = 3;
+    //   inputs[0].ki.wVk = VK_CONTROL;
+    //   inputs[1].ki.wVk = VK_BACK;
+    //   inputs[2].ki.wVk = VK_CONTROL;
+    //   inputs[2].ki.dwFlags = KEYEVENTF_KEYUP;
+    // }
+    // else if(caughtKey == 10) { // enter
+    //   inputs[0].ki.wVk = VK_RETURN;
+    // }
+    // else {
+    //   inputs[0].ki.wScan = caughtKey;
+    //   inputs[0].ki.dwFlags = KEYEVENTF_UNICODE;
+    // }
+      INPUT inputs[3] = {0};
+      uint8_t strokes = 1;
+      for(int i = 0; i < (sizeof(inputs)/sizeof(INPUT)); i++) {
+        inputs[i].type = INPUT_KEYBOARD;
+      }
+      inputs[0].ki.dwFlags = KEYEVENTF_SCANCODE;
+      inputs[0].ki.wScan = caughtScanCode;
+      if(keyinputinfo.eventtype == 0) {
+        inputs[0].ki.dwFlags |= KEYEVENTF_KEYUP;
+      }
 
-    UINT uSent = SendInput(strokes, inputs, sizeof(INPUT));
-    if (uSent != strokes) {
-      printf("For some reason didn't send the input\n");
-    }
-#endif
+      UINT uSent = SendInput(strokes, inputs, sizeof(INPUT));
+      if (uSent != strokes) {
+        printf("For some reason didn't send the input\n");
+      }
+// #endif
   }
   return 0;
 }
